f1afdf72cc580120021efb4e55c4476b1b2ab41b
sandipghosh <sandipgh19@gmail.com>
2017-07-23 09:15:12 +0530

    Profile Page

---
+++ HealthReport/src/pages/profile/profile.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+/**
+ * Generated class for the Profile page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-profile',
+  templateUrl: 'profile.html',
+})
+export class Profile {
+	email: any;
+	name: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Profile');
+
+    this.storage.get('email').then((val) => {
+    	this.email = val;
+  });
+
+  	this.storage.get('name').then((val) => {
+    	this.name = val;
+  	});
+  }
+
+}
